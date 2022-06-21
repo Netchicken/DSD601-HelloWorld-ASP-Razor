308b7284c4970ce39964a5af1888abf37eb02d01
Gary <Gary.Dix@enform.nz>
2022-06-22 10:53:45 +1200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld_ASP_Razor.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Result { get; set; }
+        public void OnPost()
+        {
+            Result = CalculateAnswer(Age, Name);
+
+        }
+
+        private string CalculateAnswer(int age, string name)
+        {
+            Age++;
+            return Name + " " + Age.ToString();
+
+        }
+
+
+
+
+        //adds data when the page is loded
+        public void OnGet()
+        {
+        }
+        //processes the page when you hit submit
+
+
+
+
+
+    }
+}
